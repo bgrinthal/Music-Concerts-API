ea0de892c17e4da0d95edd184e0ac6957a39fcb4
Ben Grinthal <bgrinthal@gmail.com>
2022-06-03 14:11:52 -0400

    added weather forcast for searched concert location

--- assets/js/script.js
+++ assets/js/script.js
@@ -295,7 +295,112 @@ fetch('https://billboard-api2.p.rapidapi.com/hot-100?range=1-10&date=2022-06-02'
     renderBillboard(response.content);
   });
   
+// universal variables
+var searchFormEl = document.querySelector('#search-form');
+var currentWeather = document.querySelector('#current-weather');
+var locationInfo = document.querySelector('#location');
+var tempInfo = document.querySelector('#temp-info');
+var windInfo = document.querySelector('#wind-info');
+var humidityInfo = document.querySelector('#humidity-info');
+var uviInfo = document.querySelector('#uvi-info');
+var recentSearch = document.querySelector('#searches');
+var searchInput = document.querySelector('#search')
 
+
+var searchHistory = [];
+
+var APIkey = "91d57b7fd6726c36fc275a86a5361130";
+
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    // grabs search input and stores in variable
+    var searchInputVal = searchInput.value;
+
+    searchHistory.push(searchInputVal);
+    localStorage.setItem('City Name', JSON.stringify(searchHistory));
+
+    // checks for valid search input
+    if (!searchInputVal) {
+        console.error('You need a search input value!');
+        return;
+    }
+
+    var queryString = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputVal + "&appid=" + APIkey + "&units=imperial"
+
+    // first api fetch for current weather info
+    fetch(queryString, {
+        cache: 'reload',
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data.name);
+            console.log(data.main.temp);
+            console.log(data.wind.speed);
+            console.log(data.main.humidity);
+            console.log(data.coord.lat);
+            console.log(data.coord.lon);
+
+            // current weather variables navigated using the API
+            var currentLat = data.coord.lat;
+            var currentLon = data.coord.lon;
+
+            var latlonString = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentLat + "&lon=" + currentLon + "&exclude=hourly&units=imperial&appid=" + APIkey;
+
+            // second api fetch for daily weather info
+            fetch(latlonString, {
+                cache: 'reload',
+            })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    console.log(data.daily[1].temp.day);
+                    console.log(data.daily[1].wind_speed);
+                    console.log(data.daily[1].humidity);
+                    console.log("https://openweathermap.org/img/w/" + data.daily[1].weather[0].icon + ".png");
+
+                    // set time for 5 day forecast
+                    for (let i = 1; i < 6; i++) {
+                        var dailyTime = document.getElementById("daily").children[i - 1].children[0];
+                        dailyTime.textContent = moment().add(i, 'days').format("MM/DD/YYYY");
+                    }
+
+                    // storing 5 day weather info iteration
+                    for (let i = 1; i < 6; i++) {
+
+                        // all daily weather variables pulled from API
+                        var dailyIconReset = document.getElementById("daily").children[i - 1].children[1];
+                        var dailyTemp = document.getElementById("daily").children[i - 1].children[2];
+                        var dailyWind = document.getElementById("daily").children[i - 1].children[3];
+                        var dailyHumidity = document.getElementById('daily').children[i - 1].children[4];
+
+                        // pull icon 
+                        var dailyIcon = data.daily[i].weather[0].icon;
+                        var img = document.createElement('img');
+                        img.src = "http://openweathermap.org/img/w/" + dailyIcon + ".png";
+
+                        console.log(daily);
+
+                        // displaying 5 day weather info
+                        dailyTemp.textContent = "Temp: " + data.daily[i].temp.day + " F";
+                        dailyWind.textContent = "Wind: " + data.daily[i].wind_speed + " mph";
+                        dailyHumidity.textContent = "Humidity: " + data.daily[i].humidity + " %";
+                        dailyIconReset.textContent = " "
+                        document.getElementById('icon-' + i).appendChild(img);
+                    }
+                })
+        })
+}
+
+console.log(searchHistory);
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
 start();
 
